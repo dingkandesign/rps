29722d5ddf77b4025687c2c2b5d84a344ca5f3b5
Dan King <danielrobertking@gmail.com>
2023-07-18 14:40:21 -0700

    Establish btn event listeners to log playerChoice

--- script.js
+++ script.js
@@ -4,59 +4,61 @@ const choiceArray = ['ROCK', 'PAPER', 'SCISSORS'];
 let computerWins = 0;
 let playerWins = 0;
 let ties = 0;
+let playerChoice = 0;
 
 //generate computer choice; make it random choice of three; uppercase for comparison
 function getComputerChoice() {
-    return choiceArray[Math.floor(Math.random()*choiceArray.length)].toUpperCase();
+  return choiceArray[Math.floor(Math.random()*choiceArray.length)].toUpperCase();
 }
-//play round
+
+function assignPlayerChoice(e) {
+	playerChoice = this.innerText.toUpperCase();
+	console.log(playerChoice);
+	playRound();
+}
+
 function playRound() {
-    let computerChoice = getComputerChoice();
-    //prompt player choice
-    let playerChoice = prompt('Rock, paper, scissors, shoot!').toUpperCase();
-    //make sure player chose an acceptable choice
-    let playerApprovedChoice = choiceArray.some(choice => playerChoice.includes(choice));
-    //prompt player if their choice is NOT acceptable
-    while (!playerApprovedChoice) {
-        playerChoice = prompt('Please enter a valid choice: rock, paper, or scissors').toUpperCase();
-        playerApprovedChoice = choiceArray.some(choice => playerChoice.includes(choice));
-    }
-    //check result, notify round result, update win counters
-    switch (true) {
-        case (playerChoice === computerChoice):
-            alert('Tie');
-            ties += 1;
-            return 'Tie';
-            break;
-        case (playerChoice === 'ROCK' && computerChoice === 'PAPER'):
-        case (playerChoice === 'PAPER' && computerChoice === 'SCISSORS'):
-        case (playerChoice === 'SCISSORS' && computerChoice === 'ROCK'):
-            alert ('Computer wins');
-            computerWins += 1;
-            return 'Computer Wins';
-            break;
-        case (playerChoice === 'ROCK' && computerChoice === 'SCISSORS'):
-        case (playerChoice === 'PAPER' && computerChoice === 'ROCK'):
-        case (playerChoice === 'SCISSORS' && computerChoice === 'PAPER'):
-            alert ('Player wins');
-            playerWins += 1;
-            return 'Player wins';
-            break;
-    }
+	let computerChoice = getComputerChoice();
+	//prompt player choice
+/*	let playerChoice = prompt('Rock, paper, scissors, shoot!').toUpperCase();
+	//make sure player chose an acceptable choice
+	let playerApprovedChoice = choiceArray.some(choice => playerChoice.includes(choice));
+	//prompt player if their choice is NOT acceptable
+	while (!playerApprovedChoice) {
+		playerChoice = prompt('Please enter a valid choice: rock, paper, or scissors').toUpperCase();
+		playerApprovedChoice = choiceArray.some(choice => playerChoice.includes(choice));
+	}
+	*/
+
+	//check result, notify round result, update win counters
+	switch (true) {
+		case (playerChoice === computerChoice):
+			alert('Tie');
+			ties += 1;
+			return 'Tie';
+			break;
+		case (playerChoice === 'ROCK' && computerChoice === 'PAPER'):
+		case (playerChoice === 'PAPER' && computerChoice === 'SCISSORS'):
+		case (playerChoice === 'SCISSORS' && computerChoice === 'ROCK'):
+			alert ('Computer wins');
+			computerWins += 1;
+			return 'Computer Wins';
+			break;
+		case (playerChoice === 'ROCK' && computerChoice === 'SCISSORS'):
+		case (playerChoice === 'PAPER' && computerChoice === 'ROCK'):
+		case (playerChoice === 'SCISSORS' && computerChoice === 'PAPER'):
+			alert ('Player wins');
+			playerWins += 1;
+			return 'Player wins';
+			break;
+	}
 
 }
-console.log(playRound());
-//play 5 round game
-function game() {
-    playRound();
-    playRound();
-    playRound();
-    playRound();
-    playRound();
-}
 
-//execute game
-game();
+const buttons = document.querySelectorAll('button');
 
-//final results
-alert('Computer: ' + computerWins + '\nPlayer: ' + playerWins + '\nTies: ' + ties);
+buttons.forEach((button) => { 
+	button.addEventListener('click', assignPlayerChoice);
+
+});
+
